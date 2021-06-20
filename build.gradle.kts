793ac5e158e8fe230841741dbc87920fe943c024
kotx <developer.hazuku@gmail.com>
2021-06-20 20:57:28 +0900

    update for development server

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,4 +37,60 @@ tasks {
             }
         }
     }
+
+    create<Copy>("buildPlugin") {
+        group = "plugin"
+        from(jar)
+        val dest = File(projectDir, "server/plugins")
+        if (File(dest, jar.get().archiveFileName.get()).exists()) dest.delete()
+        into(dest)
+    }
+
+    create<DefaultTask>("setupWorkspace") {
+        group = "plugin"
+        doLast {
+            val paperDir = File(projectDir, "server")
+
+            paperDir.mkdirs()
+
+            val download by registering(de.undercouch.gradle.tasks.download.Download::class) {
+                src("https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/576/downloads/paper-1.16.5-576.jar")
+                dest(paperDir)
+            }
+            val paper = download.get().outputFiles.first()
+
+            download.get().download()
+
+            runCatching {
+                javaexec {
+                    workingDir(paperDir)
+                    main = "-jar"
+                    args("./${paper.name}", "nogui")
+                }
+
+                val eula = File(paperDir, "eula.txt")
+                eula.writeText(eula.readText(Charsets.UTF_8).replace("eula=false", "eula=true"), Charsets.UTF_8)
+                val serverProperties = File(paperDir, "server.properties")
+                serverProperties.writeText(
+                    serverProperties.readText(Charsets.UTF_8)
+                        .replace("online-mode=true", "online-mode=false")
+                        .replace("difficulty=easy", "difficulty=peaceful")
+                        .replace("spawn-protection=16", "spawn-protection=0")
+                        .replace("gamemode=survival", "gamemode=creative")
+                        .replace("level-name=world", "level-name=dev_world")
+                        .replace("level-type=default", "level-type=flat")
+                        .replace("motd=A Minecraft Server", "motd=Kotx Development Server")
+                        .replace("max-tick-time=60000", "max-tick-time=-1")
+                        .replace("view-distance=10", "view-distance=16"), Charsets.UTF_8
+                )
+                val runBat = File(paperDir, "run.bat")
+                if (!runBat.exists()) {
+                    runBat.createNewFile()
+                    runBat.writeText("java -jar ./${paper.name} nogui", Charsets.UTF_8)
+                }
+            }.onFailure {
+                it.printStackTrace()
+            }
+        }
+    }
 }
